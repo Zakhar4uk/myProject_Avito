43a14aa89ec94e75f6a0c34e093f82cc40bb6a9d
MichaelZak <zakharchuk1991@mail.ru>
2024-09-28 16:25:18 +0300

    Добавление Тест-кейса

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,7 +11,7 @@ abstract public class BaseTest {
     public void setUp() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = true;
+        Configuration.headless = false;
     }
 
     @Before
